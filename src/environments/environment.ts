e3dc998d4427aa8638d6c202dae51157975c2970
Vaishu <svaish2000@gmail.com>
2022-06-22 12:33:26 +0530

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: 'us-east-1_lQ6qnYCfU',
-    userPoolWebClientId: '1bat195arc387ltabp285vtlj6',
+    userPoolId: '',
+    userPoolWebClientId: '',
   },
   region: 'us-east-1',
-  accessKeyId: 'AKIA5A455FMMYAYNVDEA',
-  secretAccessKey: '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',
+  accessKeyId: '',
+  secretAccessKey: '',
 };
 
 /*
